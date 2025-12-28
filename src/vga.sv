e4b5248d9be324d1ac25e2c102fab20abf9f661c
David Volz <volz.david@gmx.de>
2025-12-29 00:08:48 +0100

    smaller vga implementation

--- src/vga.sv
+++ src/vga.sv
@@ -65,18 +65,23 @@ module vga (
 		.hsync(hsync)
 	);
 
+	parameter int BUFFER_WIDTH = (GAME_WIDTH/2);
+	parameter [5:0] ROW_OFFSET = 25;
+
 	logic [1:0] sx;
 	logic [1:0] sy;
 	logic [1:0] prev_dir;
 	logic [3:0] decode_dir;
 	logic [3:0] decode_prev_dir;
-	logic [3:0] row_buffer [GAME_WIDTH-1:0];
+	logic [3:0] two_hot_dir;
+	logic [3:0] row_buffer [BUFFER_WIDTH-1:0];
 
 	always @(*) begin
 		decode_dir = 0;
-		decode_dir[snake_dir] = 1;
+		decode_dir[snake_dir] = ~snake_last;
 		decode_prev_dir = 0;
-		decode_prev_dir[prev_dir] = 1;
+		decode_prev_dir[prev_dir] = ~snake_first;
+		two_hot_dir = decode_prev_dir | decode_dir;
 	end
 
 	always @(*) begin
@@ -89,29 +94,27 @@ module vga (
 				2'b01: rgb = 6'b100000;
 				default: rgb = 6'b111111;
 			endcase
-		end else begin
-			if ({sx, sy} == 4'b0101 && row_buffer[tx-1] != 0) begin
-				// snake center
-				rgb = 6'b001100;
-			end else if ({sx, sy} == 4'b0101 && tx == apple_x && ty == apple_y && apple_valid) begin
-				// apple center
-				rgb = 6'b110000;
-			end else if ({sx, sy} == 4'b0110 && row_buffer[tx-1][0]) begin
-				// top-center
-				rgb = 6'b001100;
-			end else if ({sx, sy} == 4'b0100 && row_buffer[tx-1][1]) begin
-				// bottom-center
-				rgb = 6'b001100;
-			end else if ({sx, sy} == 4'b1001 && row_buffer[tx-1][2]) begin
-				// left-center
-				rgb = 6'b001100;
-			end else if ({sx, sy} == 4'b0001 && row_buffer[tx-1][3]) begin
-				// right-center
-				rgb = 6'b001100;
-			end else /*if (row_buffer[tx-1] == 0 || {sx, sy} == 4'b0000 || {sx, sy} == 4'b1000 || {sx, sy} == 4'b0010 || {sx, sy} == 4'b1010)*/ begin
-				// no snake or corner
-				rgb = 6'b000000;
-			end
+		end else if ({sx, sy} == 4'b0101 && row_buffer[0] != 0) begin
+			// snake center
+			rgb = 6'b001100;
+		end else if ({sx, sy} == 4'b0101 && tx == apple_x && ty == apple_y && apple_valid) begin
+			// apple center
+			rgb = 6'b110000;
+		end else if ({sx, sy} == 4'b0110 && row_buffer[0][0]) begin
+			// top-center
+			rgb = 6'b001100;
+		end else if ({sx, sy} == 4'b0100 && row_buffer[0][1]) begin
+			// bottom-center
+			rgb = 6'b001100;
+		end else if ({sx, sy} == 4'b1001 && row_buffer[0][2]) begin
+			// left-center
+			rgb = 6'b001100;
+		end else if ({sx, sy} == 4'b0001 && row_buffer[0][3]) begin
+			// right-center
+			rgb = 6'b001100;
+		end else /*if (row_buffer[0] == 0 || {sx, sy} == 4'b0000 || {sx, sy} == 4'b1000 || {sx, sy} == 4'b0010 || {sx, sy} == 4'b1010)*/ begin
+			// no snake or corner
+			rgb = 6'b000000;
 		end
 	end
 
@@ -130,10 +133,20 @@ module vga (
 	end
 
 	always @(posedge clk) begin
-		if (ty != next_ty && tx != next_tx && visible) begin
-			row_buffer[tx-1] <= 2'b00;
-		end else if (snake_valid && snake_y == next_ty && (snake_x < tx || ty == next_ty)) begin
-			row_buffer[snake_x-1] <= (decode_prev_dir & ~{snake_first, snake_first, snake_first, snake_first}) | (decode_dir & ~{snake_last, snake_last, snake_last, snake_last});
+		if (snake_valid) begin
+			if (snake_y == ty && snake_x > tx && snake_x < tx + BUFFER_WIDTH) begin
+				// same row
+				row_buffer[snake_x-tx] = two_hot_dir;
+			end else if (snake_y == next_ty && snake_x + ROW_OFFSET > tx && snake_x + ROW_OFFSET < tx + BUFFER_WIDTH) begin
+				// next row
+				row_buffer[snake_x+ROW_OFFSET-tx] = two_hot_dir;
+			end
+		end
+		if (px[4:0] == 31) begin
+			for (int i = 0; i < BUFFER_WIDTH-1; i = i + 1) begin
+				row_buffer[i] = row_buffer[i+1];
+			end
+			row_buffer[BUFFER_WIDTH-1] = 0;
 		end
 	end
 
