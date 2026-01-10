4f3eb110adad12968ddc85bcf88f3a0024417292
David Volz <volz.david@gmx.de>
2026-01-10 10:21:32 +0100

    rough pwm outline

--- src/sound.sv
+++ src/sound.sv
@@ -18,6 +18,40 @@ module sound (
     output logic       audio
 );
 
-    assign audio = 0;
+    logic [1:0] mode;
+    logic [1:0] next_mode;
+    logic [7:0] counter;
+    logic [7:0] next_counter;
+    logic [7:0] max_counter;
+    logic [7:0] next_max_counter;
+
+    always @(*) begin
+        next_mode = mode;
+        casez ({ mode, vsync, tick, eat, success, failure })
+        7'b00?_????: 
+        endcase
+    end
+
+    always @(*) begin
+        case (mode)
+            1: begin
+
+            end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            mode <= 0;
+            counter <= 0;
+            max_counter <= 0;
+            audio <= 0;
+        end else begin
+            counter <= (counter == max_counter) ? 0 : counter + 1;
+            if (counter == max_counter) begin
+                audio <= !audio;
+            end
+        end
+    end
 
 endmodule
